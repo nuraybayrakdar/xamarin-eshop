c8b7c73ce4a2afa555a4406ccb372410d19f7c13
alyilmaz99 <73197677+alyilmaz99@users.noreply.github.com>
2022-06-19 02:56:43 +0300

    accountpageviewmodel added

---
+++ eShopOnContainers/eShopOnContainers.Core/ViewModels/AccountPageViewModel.cs
@@ -0,0 +1,33 @@
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Text;
+using Xamarin.Forms;
+using eShopOnContainers.Core.Models.User;
+using eShopOnContainers.Core.Service.User;
+using System.Threading.Tasks;
+
+namespace eShopOnContainers.Core.ViewModels
+{
+    class AccountPageViewModel : BaseViewModel
+    {
+        public Command LogoutCommand { get; }
+        public string Username { get; set; }
+
+        public AccountPageViewModel()
+        {
+            LogoutCommand = new Command(Logout);
+            if (CurrentAccount() != null)
+            {
+                Username = CurrentAccount().Username;
+            }
+        }
+
+        private async void Logout()
+        {
+            App.Current.MainPage = new AppShell();
+            await Shell.Current.GoToAsync($"///{nameof(Anasayfa)}");
+        }
+    }
+}
