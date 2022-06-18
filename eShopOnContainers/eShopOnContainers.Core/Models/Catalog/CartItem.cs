45a2e5359b71fa75c94e6e70c274a7b63f41a7fb
nuray <83471802+nuraybayrakdar@users.noreply.github.com>
2022-06-19 02:02:50 +0300

    CartItemcs added

---
+++ eShopOnContainers/eShopOnContainers.Core/Models/Catalog/CartItem.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using SQLite;
+
+namespace Navigation2.Models
+{
+    public class CartItem
+    {
+        [PrimaryKey, AutoIncrement]
+        public int Id { get; set; }
+        public Product Product { get; set; }
+        public int Amount { get; set; }
+        public float Total { get; set; }
+
+        public CartItem(Product product, int amount)
+        {
+            Product = product;
+            Amount = amount;
+            Total = 0;
+        }
+
+        public CartItem()
+        {
+
+        }
+    }
+}
