a5a65cd0123f02aacb42938bf1c1e93f2d9d07b4
nuray <83471802+nuraybayrakdar@users.noreply.github.com>
2022-06-19 03:18:26 +0300

    SearchPageViewModelcs added

---
+++ eShopOnContainers/eShopOnContainers.Core/ViewModels/SearchPageViewModel.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Text;
+using Xamarin.Forms;
+using System.Threading.Tasks;
+using eShopOnContainers.Core.Views;
+using eShopOnContainers.Core.Models;
+using eShopOnContainers.Core.Services.DataHolder;
+using System.Threading;
+
+namespace eShopOnContainers.Core.ViewModels
+{
+    class SearchPageViewModel : BaseViewModel
+    {
+        public string Keyword { get; set; }
+        public Command SearchCommand { get; }
+
+        public SearchPageViewModel()
+        {
+            SearchCommand = new Command(SearchByName);
+        }
+
+        private async void SearchByName()
+        {
+            DataHolder.GetProductsByName(Keyword);
+            await Shell.Current.GoToAsync($"///{nameof(Anasayfa)}/{nameof(AramaSonucu)}");
+        }
+
+    }
+}
