b2a3014cb3b3d956ba366468993044ea628a1ce2
alyilmaz99 <73197677+alyilmaz99@users.noreply.github.com>
2022-06-19 03:13:19 +0300

    kzkulaklikviewmodel added

---
+++ eShopOnContainers/eShopOnContainers.Core/ViewModels/KZKulaklikViewModel.cs
@@ -0,0 +1,21 @@
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Text;
+using Xamarin.Forms;
+using eShopOnContainers.Core.Service.DataHolder;
+using eShopOnContainers.Core.Models.Catalog;
+
+namespace eShopOnContainers.Core.ViewModels
+{
+    class KZKulaklikViewModel : BaseViewModel
+    {
+        public KZKulaklikViewModel()
+        {
+            DataHolder.GetProductsByCategory("kulaklýk");
+            Products = DataHolder.FilteredList;
+        }
+
+    }
+}
