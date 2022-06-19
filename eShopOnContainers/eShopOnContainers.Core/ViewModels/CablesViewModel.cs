b5fa13ab73a88bcc3fb63ebc1d4a6011eee19e4a
nuray <83471802+nuraybayrakdar@users.noreply.github.com>
2022-06-19 04:03:21 +0300

    CablesViewModelcs added

---
+++ eShopOnContainers/eShopOnContainers.Core/ViewModels/CablesViewModel.cs
@@ -0,0 +1,18 @@
+﻿using eShopOnContainers.Core.Models;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Text;
+using eShopOnContainers.Core.Services.DataHolder;
+
+namespace eShopOnContainers.Core.ViewModels
+{
+    class CablesViewModel : BaseViewModel
+    {
+        public CablesViewModel()
+        {
+            DataHolder.GetProductsByCategory("kablo");
+            Products = DataHolder.FilteredList;
+        }
+    }
+}
