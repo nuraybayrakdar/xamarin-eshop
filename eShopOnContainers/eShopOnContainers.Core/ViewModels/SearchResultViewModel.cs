acb7d3403a7eff7f705ae60794932ecc903d43d9
nuray <83471802+nuraybayrakdar@users.noreply.github.com>
2022-06-19 03:26:01 +0300

    SearchResultPageViewModelcs added

---
+++ eShopOnContainers/eShopOnContainers.Core/ViewModels/SearchResultViewModel.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Text;
+using Xamarin.Forms;
+using System.Threading.Tasks;
+using eShopOnContainers.Core.Views;
+using eShopOnContainers.Core.Models;
+using eShopOnContainers.Core.Services.DataHolder;
+using System.Threading;
+
+namespace eShopOnContainers.Core.ViewModels
+{
+    class SearchResultViewModel : BaseViewModel
+    {
+        public SearchResultViewModel()
+        {
+            Products = DataHolder.FilteredList;
+        }
+    }
+}
