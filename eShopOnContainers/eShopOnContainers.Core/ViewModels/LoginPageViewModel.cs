dab1b15ac74a8b5f77a10638cbed2646b24c1a9f
nuray <83471802+nuraybayrakdar@users.noreply.github.com>
2022-06-19 03:39:44 +0300

    LoginPageVievModelcs added

---
+++ eShopOnContainers/eShopOnContainers.Core/ViewModels/LoginPageViewModel.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Text;
+using Xamarin.Forms;
+using System.Threading.Tasks;
+using eShopOnContainers.Core.Views;
+using eShopOnContainers.Core.Models;
+using eShopOnContainers.Core.Services.FirebaseManager;
+
+namespace eShopOnContainers.Core.ViewModels
+{
+    public class LoginPageViewModel : BaseViewModel
+    {
+
+        public string Username { get; set; }
+        public string RegisterUsername { get; set; }
+        public string Password { get; set; }
+        public string RegisterPassword { get; set; }
+
+        public Command LoginCommand { get; }
+        public Command RegisterCommand { get; }
+
+        public LoginPageViewModel()
+        {
+            LoginCommand = new Command(Login);
+            RegisterCommand = new Command(Register);
+        }
+
+        public async void Login()
+        {
+            Account LoginAccount = new Account(Username, Password);
+            //var AccountList = await DatabaseManager.GetAccount();
+            var AccountList = await FirebaseManager.GetAccount();
+            foreach (Account account in AccountList)
+            {
+                if (account.Username == LoginAccount.Username && account.Password == LoginAccount.Password)
+                {
+                    SetCurrentAccount(LoginAccount);
+                    AppShell denemeShell = new AppShell();
+                    denemeShell.Login();
+                    App.Current.MainPage = denemeShell;
+                    await Shell.Current.GoToAsync($"///{nameof(Anasayfa)}");
+                }
+            }
+        }
+
+        private async void Register()
+        {
+            Account RegisterAccount = new Account(RegisterUsername, RegisterPassword);
+            //await DatabaseManager.AddAccount(RegisterAccount);
+
+            await FirebaseManager.AddAccount(RegisterAccount);
+        }
+
+
+    }
+}
