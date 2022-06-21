71ef62b3a73ddf2ee4d1dae074d154a2a6b87fac
alyilmaz99 <73197677+alyilmaz99@users.noreply.github.com>
2022-06-22 00:12:56 +0300

    appshell added

---
+++ eShopOnContainers/eShopOnContainers.Core/AppShell.xaml.cs
@@ -0,0 +1,26 @@
+using Xamarin.Forms;
+using eShopOnContainers.Core.ViewModels;
+using eShopOnContainers.Core.Views;
+using eShopOnContainers.Core.Services;
+
+namespace eShopOnContainers.Core
+{
+    public partial class AppShell : Xamarin.Forms.Shell
+    {
+
+        public AppShell()
+        {
+            InitializeComponent();
+            AccountPage.IsVisible = false;
+            Routing.RegisterRoute(nameof(Sepet), typeof(Sepet));
+            Routing.RegisterRoute(nameof(AramaSayfasi), typeof(AramaSayfasi));
+            Routing.RegisterRoute(nameof(AramaSonucu), typeof(AramaSonucu));
+            Routing.RegisterRoute(nameof(SatinAlindi), typeof(SatinAlindi));
+        }
+        public void Login()
+        {
+            LoginPage.IsVisible = false;
+            AccountPage.IsVisible = true;
+        }
+    }
+}
